a51521cda0970ad53db446453b9bddf5d5ee952c
NicKAnderson038 <nickanderson@gmail.com>
2018-01-15 18:56:55 -0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app light>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2018</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fixed: false,
+      title: "Vuetify.js"
+    };
+  }
+};
+</script>
---
+++ src/components/Hello.vue
@@ -0,0 +1,69 @@
+<template>
+   <v-data-table
+      v-bind:headers="headers"
+      :items="items"
+      hide-actions
+      class="elevation-1"
+    >
+    <template slot="items" slot-scope="props">
+      <td>{{ props.item.Name }}</td>
+      <td class="text-xs-right">{{ props.item.Email }}</td>
+      <td class="text-xs-right">{{ props.item.Phone }}</td>
+      <td class="text-xs-right">{{ props.item.Nsslha }}</td>
+      <td class="text-xs-right">{{ props.item.Csha }}</td>
+      <td class="text-xs-right">{{ props.item.Attendance }}</td>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      headers: [
+        {
+          text: "Names",
+          align: "left",
+          sortable: false,
+          value: "name"
+        },
+        { text: "Email", value: "Email" },
+        { text: "Phone", value: "Phone" },
+        { text: "Nsslha", value: "Nsslha" },
+        { text: "Csha", value: "Csha" },
+        { text: "Attendance", value: "Attendance" }
+      ],
+      items: []
+    };
+  },
+  created() {
+    try {
+      this.fetch();
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  methods: {
+    async fetch() {
+      try {
+        const response = await fetch(
+          "https://fn5nx4fsp7.execute-api.us-east-1.amazonaws.com/dev/nsslha"
+        );
+        const data = await response.json();
+        console.log("It worked! ", data);
+        this.items = data;
+        return;
+      } catch (error) {
+        throw new Error(err);
+      }
+    }
+  }
+};
+</script>
+
+<style>
+.table__overflow {
+  margin-top: 40px;
+}
+</style>
